b43d3c454dfadf6376f39a429fb044989eb3abdf
Danil Zaviryukha <logolevel@gmail.com>
2016-09-29 13:27:42 +0300

    Added accordion

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,6 @@
 $(function() {
 
+/*carousel*/
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:0,
@@ -9,5 +10,38 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+/*accordion*/
+var $linkFirst = $('.accordion > dt:first-child');
+var $allPanels = $('.accordion > dd').hide();
+var $allLinks = $('.accordion > dt > a');
+
+function openFirstPanel(){
+  $('.accordion > dt:first-child').next().addClass('active-ac').slideDown();
+}
+
+openFirstPanel();
+  
+$allLinks.click(function() {
+    $this = $(this);
+    $target =  $this.parent().next();
+    
+  
+    if($target.hasClass('active-ac')){
+      $target.removeClass('active-ac').slideUp(); 
+      $this.removeClass('active-ac-link'); 
+    }else{
+    	$linkFirst.removeClass('active-ac-link');
+      $allPanels.removeClass('active-ac').slideUp();
+      $allLinks.removeClass('active-ac-link');
+      $target.addClass('active-ac').slideDown();
+      $this.addClass('active-ac-link');
+    }
+    
+  return false;
+});
+
+
+
 
 });
---
+++ dist/js/common.js
@@ -0,0 +1,45 @@
+$(function() {
+
+/*carousel*/
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    responsive:{
+        0:{
+            items:1
+        }
+    }
+})
+
+/*accordion*/
+var $linkFirst = $('.accordion > dt:first-child');
+var $allPanels = $('.accordion > dd').hide();
+var $allLinks = $('.accordion > dt > a');
+
+function openFirstPanel(){
+  $('.accordion > dt:first-child').next().addClass('active-ac').slideDown();
+  $linkFirst.addClass('active-ac-link');
+}
+
+  openFirstPanel();
+    
+  $allLinks.click(function() {
+      $this = $(this);
+      $target =  $this.parent().next();
+      
+    
+      if($target.hasClass('active-ac')){
+        $target.removeClass('active-ac').slideUp(); 
+        $this.removeClass('active-ac-link'); 
+      }else{
+      	$linkFirst.removeClass('active-ac-link');
+        $allPanels.removeClass('active-ac').slideUp();
+        $allLinks.removeClass('active-ac-link');
+        $target.addClass('active-ac').slideDown();
+        $this.addClass('active-ac-link');
+      }
+      
+    return false;
+  });
+
+});
